b327b930a3c2b077fc1553d1f9dd901b0daaf995
EvansMaingi <Evans.Maingi@strathmore.edu>
2025-05-30 19:54:07 +0300

    Initial commit

---
+++ resources/views/home/css.blade.php
@@ -0,0 +1,15 @@
+<div class="menu">
+    <div id="logo">AgroLink</div>
+    
+    <div class="navlinks">
+        <p><a href="#">Home</a></p>
+        <p><a href="#">About</a></p>
+        <p><a href="#">Products</a></p>
+        <p><a href="#">Contact Us</a></p>
+    </div>
+
+    <div class="auth-buttons">
+        <a href="{{ route('login') }}" class="login-btn">Login</a>
+        <a href="{{ route('register') }}" class="register-btn">Register</a>
+    </div>
+</div>
